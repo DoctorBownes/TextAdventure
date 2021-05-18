89c4c28e1428e7817fa86d61aefb0db59d7f5b85
SimonR <sw.roeloffs@hotmail.com>
2021-05-18 09:49:58 +0200

    Item update 1.1

---
+++ Zuul/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuul
+{
+    class Item
+    {
+        public int Weight { get; }
+        public string Description { get; }
+        public Item(int weight, string description)
+        {
+            Weight = weight;
+            Description = description;
+        }
+    }
+}
